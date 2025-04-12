55c03c27add2137ca79fdeb51f554b9bf8f7ad30
uvusibuneka <kachanovdanilka@gmail.com>
2025-04-12 20:33:03 +0300

    Update notify_commit.py

--- notify_commit.py
+++ notify_commit.py
@@ -7,6 +7,8 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+
+
 openai.api_key = OPENAI_API_KEY
 bot = Bot(token=TELEGRAM_TOKEN)
 
